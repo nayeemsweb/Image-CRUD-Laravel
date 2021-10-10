8d153081cfe92672d11fdaa85951a1b4a09eb0ec
Nayeem Rahman <nayeem12rahman@gmail.com>
2021-10-10 20:41:30 +0600

    Delete Feature Added

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="card-header">
                     <h4>Laravel 8 Image CRUD
                     <a href="{{ url('add-student') }}" class="btn btn-primary float-end">Add Student</a>
-                    </h4>                    
+                    </h4>
                 </div>
                 <div class="card-body">
                     <table class="table table-bordered table-striped">
@@ -25,7 +25,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($student as $item)                            
+                            @foreach($student as $item)
                             <tr>
                                 <td>{{ $item->id }}</td>
                                 <td>{{ $item->name }}</td>
@@ -38,13 +38,19 @@
                                     <a href="{{ url('edit-student/'.$item->id) }}" class="btn btn-primary btn-sm">Edit</a>
                                 </td>
                                 <td>
-                                    <a href="" class="btn btn-danger btn-sm">Delete</a>
+{{--                                    <a href="{{ url('delete-student/'.$item->id) }}}" class="btn btn-danger btn-sm">Delete</a>--}}
+                                    <form action="{{ url('delete-student/'.$item->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button class="btn btn-danger btn-sm" type="submit">Delete</button>
+                                    </form>
                                 </td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
-                </div>                
+                </div>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,8 @@ Route::get('add-student', [StudentController::class, 'create']);
 Route::post('add-student', [StudentController::class, 'store']);
 Route::get('edit-student/{id}', [StudentController::class, 'edit']);
 Route::put('update-student/{id}', [StudentController::class, 'update']);
-
-
-
-
-
+//Route::get('delete-student/{id}', [StudentController::class, 'destroy']);
+Route::delete('delete-student/{id}', [StudentController::class, 'destroy']);
 
 
 Route::get('/', function () {
